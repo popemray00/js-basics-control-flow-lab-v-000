e05174e0be95a791dfa8fde499644e69751824e3
Michael Pope <popemray1986@gmail.com>
2018-06-03 16:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     return "This one is on me!";
   }
-  else if (someValue >= 2000) {
+  else if (someValue >= 2000 && someValue < 2500) {
     return 'I will gladly take your thirty bucks.'
   }
   else {
