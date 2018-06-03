2b85d44f21595e6e110d19f7e843b18256bd99ff
Michael Pope <popemray1986@gmail.com>
2018-06-03 17:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function scuberGreetingForFeet(someValue) {
 }
 
 function ternaryCheckCity(city) {
-  return city = "NYC" ? "Ok, sounds good." : "No go."
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
